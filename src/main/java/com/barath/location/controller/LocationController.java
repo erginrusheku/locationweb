59ecfd24140df82ae41cb7706ec04c1194c9f8c9
Ergin Rusheku <rushekuergin@gmail.com>
2024-02-22 18:07:19 +0100

    jsp and thymeleaf

--- src/main/java/com/barath/location/controller/LocationController.java
+++ src/main/java/com/barath/location/controller/LocationController.java
@@ -1,13 +1,65 @@
 package com.barath.location.controller;
 
+import com.barath.location.entities.Location;
+import com.barath.location.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class LocationController {
+    @Autowired
+    LocationService locationService;
 
     @RequestMapping("/showCreate")
     public String showCreate(){
         return "createLocation";
+    }
+
+    @RequestMapping("/saveLoc")
+    public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap){
+       Location locationSaved = locationService.saveLocation(location);
+       String msg = "Location saved with id: " + locationSaved.getId();
+       modelMap.addAttribute("msg", msg);
+        return "createLocation";
+    }
+
+    @RequestMapping("/displayLocations")
+    public String displayLocations(ModelMap modelMap){
+        List<Location> locations = locationService.getAllLocations();
+        modelMap.addAttribute("locations", locations);
+        return "displayLocations";
+    }
+
+    @RequestMapping("/deleteLocation")
+    public String deleteLocation(@RequestParam("id") int id, ModelMap modelMap){
+        Location location = new Location();
+        location.setId(id);
+        locationService.deleteLocation(location);
+        List<Location> locations = locationService.getAllLocations();
+        modelMap.addAttribute("locations", locations);
+
+        return "displayLocations";
+    }
+    @RequestMapping("/showUpdate")
+    public String showUpdate(@RequestParam("id") int id, ModelMap modelMap){
+        Location location = locationService.getLocationById(id);
+        modelMap.addAttribute("location", location);
+        return "updateLocation";
+    }
+
+    @RequestMapping("/updateLoc")
+    public String updateLocation(@ModelAttribute("location")Location location, ModelMap modelMap){
+        locationService.updateLocation(location);
+
+        List<Location> locations = locationService.getAllLocations();
+        modelMap.addAttribute("locations", locations);
+
+        return "displayLocations";
     }
 }
