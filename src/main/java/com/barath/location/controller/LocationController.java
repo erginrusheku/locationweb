f8ad5bea536923631e904915677de9e095b8a9ca
Ergin Rusheku <rushekuergin@gmail.com>
2024-02-23 02:40:17 +0100

    email,chart

--- src/main/java/com/barath/location/controller/LocationController.java
+++ src/main/java/com/barath/location/controller/LocationController.java
@@ -1,20 +1,33 @@
 package com.barath.location.controller;
 
 import com.barath.location.entities.Location;
+import com.barath.location.repository.LocationRepository;
 import com.barath.location.service.LocationService;
+import com.barath.location.util.ReportUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.barath.location.util.EmailUtil;
 
+
+import javax.servlet.ServletContext;
 import java.util.List;
 
 @Controller
 public class LocationController {
     @Autowired
-    LocationService locationService;
+    private LocationService locationService;
+    @Autowired
+    LocationRepository locationRepository;
+    @Autowired
+    private EmailUtil emailUtil;
+    @Autowired
+    ReportUtil reportUtil;
+    @Autowired
+    ServletContext sc;
 
     @RequestMapping("/showCreate")
     public String showCreate(){
@@ -26,6 +39,8 @@ public class LocationController {
        Location locationSaved = locationService.saveLocation(location);
        String msg = "Location saved with id: " + locationSaved.getId();
        modelMap.addAttribute("msg", msg);
+       emailUtil.sendEmail("rushekuergin@com","Location Saved",
+               "Good");
         return "createLocation";
     }
 
@@ -61,5 +76,14 @@ public class LocationController {
         modelMap.addAttribute("locations", locations);
 
         return "displayLocations";
+    }
+
+    @RequestMapping("/generateReport")
+    public String generateReport(){
+        String path = sc.getRealPath("/");
+
+        List<Object[]> data = locationRepository.findTypeAndTypeCount();
+        reportUtil.generatePieChart(path,data);
+        return "report";
     }
 }
--- src/main/java/com/barath/location/repository/LocationRepository.java
+++ src/main/java/com/barath/location/repository/LocationRepository.java
@@ -2,6 +2,12 @@ package com.barath.location.repository;
 
 import com.barath.location.entities.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+    @Query("select type,count(type) from Location group by type")
+    public List<Object[]> findTypeAndTypeCount();
 }
